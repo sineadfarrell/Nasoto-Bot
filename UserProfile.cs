a59278372db037e13c667f78fe0cd2b2f12adb61
SineadFarrell <sinead.farrell1@ucdconnect.ie>
2020-03-14 17:48:58 +0000

    minor fixes

--- UserProfile.cs
+++ UserProfile.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class UserProfile
 {
     public string[] Name { get; set; }
+    public string[] Activity {get; set;} 
     // public string[] NumberOfModules { get; set; }
     // public string[] Module { get; set; }
     // public string[] Stage { get; set; }
